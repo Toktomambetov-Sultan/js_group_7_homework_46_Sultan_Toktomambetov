4f84cce42d4caa88378a98c0dbdf702de6af737b
Toktomambetov Sultan <sultan.toktomambetov.04@bk.ru>
2020-07-08 14:40:59 +0600

    did task2

--- task2/index.js
+++ task2/index.js
@@ -1,5 +1,5 @@
 const figlet = require('figlet');
-figlet('Hello World!!', function(err, data) {
+figlet(process.argv[2], function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
